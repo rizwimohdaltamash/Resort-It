f9d8340fa49ae5f325cc627ada529d9ca949fd1a
AltamashRizwi <rizwimohdaltamash@gmail.com>
2024-09-04 14:19:38 +0530

    code updated

--- src/pages/gemini/Gemini.jsx
+++ src/pages/gemini/Gemini.jsx
@@ -11,7 +11,7 @@ const Gemini = () => {
         onClick={() => {
           navigate("/gemini");
         }}
-        className="fixed right-4 bottom-32 lg:bottom-36 w-[10%] lg:w-[7%] border-2 border-[#00a99d] bg-gray-300 rounded-full py-1 lg:py-3 shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:scale-110 z-40"
+        className="fixed right-4 bottom-32 lg:bottom-36 w-[20%] lg:w-[7%] border-2 border-[#00a99d] bg-gray-300 rounded-full py-2 lg:py-3 shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:scale-110 z-40"
       >
         <Lottie animationData={Bot} />
       </button>
